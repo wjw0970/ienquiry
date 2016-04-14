bff0b372cca796f98540b2b939baa27b9329d04c
Wang Jiawen <wang.jiawen@shahids-MacBook-Pro.local>
2016-04-14 10:41:17 +0800

    commit local files

---
+++ src/com/app/ienquiry/Ienquiry.java
@@ -0,0 +1,57 @@
+package com.app.ienquiry;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Ienquiry {
+
+	// final static Logger logger = Logger.getLogger(OpenBrowser.class);
+
+	public static void launchBrowser() {
+
+		// ReadProperties data = new ReadProperties();
+		WebDriver d = null;
+		try {
+			d = new FirefoxDriver();
+			d.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+			d.get("https://ienquiry.ica.gov.sg/home.do");
+			Thread.sleep(3000);
+			WebElement loginSingpass = d.findElement(By.cssSelector("#main > div:nth-child(13) > a:nth-child(1)"));
+			loginSingpass.click();
+			Thread.sleep(3000);
+			WebElement LoginName = d.findElement(By.id("loginID"));
+			LoginName.click();
+			LoginName.sendKeys("");
+			WebElement Password = d.findElement(By.id("password"));
+			Password.sendKeys("");
+			WebElement loginBtn = d.findElement(
+					By.cssSelector("#personBean > div:nth-child(4) > button.pure-button.pure-button-primary.btn"));
+			loginBtn.click();
+			Thread.sleep(5000);
+			WebElement statusValue = d
+					.findElement(By.cssSelector("#ownAppSection > table > tbody > tr.bodyTxt > td:nth-child(3)"));
+
+			String result = statusValue.getText();
+			System.out.println("The status is " + result);
+
+			WebElement logout = d.findElement(By.cssSelector("#spLogout > a"));
+			logout.click();
+
+			Alert a = d.switchTo().alert();
+			a.accept();
+			Thread.sleep(3000);
+			d.quit();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
---
+++ src/com/app/ienquiry/Run.java
@@ -0,0 +1,9 @@
+package com.app.ienquiry;
+
+public class Run {
+
+	public static void main(String[] args) {
+		Ienquiry.launchBrowser();
+	}
+
+}
